040238b1addb1fb8635fdbecd6b8477eb6e11d44
Dragos Tudor <dragos.tudor@gmail.com>
2024-05-20 17:58:46 +0000

    change(sql): update test sql version.

--- Storing.SqlServer/TestContainers/Starting.Tests.cs
+++ Storing.SqlServer/TestContainers/Starting.Tests.cs
@@ -6,7 +6,7 @@ namespace Storing.SqlServer;
 static partial class TestContainers
 {
   const string ContainerName = "storing-sql";
-  const string ImageName = "mcr.microsoft.com/mssql/server:2017-latest";
+  const string ImageName = "mcr.microsoft.com/mssql/server:2019-latest";
 
   static readonly TimeSpan DockerTimeout = TimeSpan.FromMinutes(10);
   static readonly TimeSpan OpenPortTimeout = TimeSpan.FromMinutes(3);
@@ -23,7 +23,10 @@ static partial class TestContainers
     using var cts = new CancellationTokenSource(dockerTimeout ?? DockerTimeout);
 
     await CreateDockerImageAsync(client.Images, imageName, cts.Token);
-    var containerId = await UseContainerAsync(client.Containers, imageName, containerName, (@params) => @params.Env = ["ACCEPT_EULA=Y", $"SA_PASSWORD={adminPassword}"], cts.Token);
+    var containerId = await UseContainerAsync(client.Containers, imageName, containerName, (@params) => {
+      @params.Env = ["ACCEPT_EULA=Y", $"SA_PASSWORD={adminPassword}"];
+      @params.Volumes = new Dictionary<string, EmptyStruct>() { { "storingsql:/var/opt/mssql", new EmptyStruct() } };
+    }, cts.Token);
     var container = await InspectContainerAsync(client.Containers, containerId, cts.Token);
 
     await WaitForOpenPort(client.Exec, containerId, serverPort, openPortTimeout ?? OpenPortTimeout);
