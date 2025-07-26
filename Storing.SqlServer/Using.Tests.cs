6ba9d6cc02be222a23e88cad19aadf630265c0dc
Dragos Tudor <dragos.tudor@gmail.com>
2025-07-26 19:36:49 +0000

    refactor(sql): remove unneccessary cancelation token;

--- Storing.SqlServer/Using.Tests.cs
+++ Storing.SqlServer/Using.Tests.cs
@@ -10,7 +10,7 @@ public partial class SqlServerTests
   static readonly string EntitiesConnString = CreateSqlConnectionString("entities", "sa", "P@ssw0rd!", "127.0.0.1");
   static readonly string TrackingConnString = CreateSqlConnectionString("tracking", "sa", "P@ssw0rd!", "127.0.0.1");
 
-  static void InitializeDatabase<TContext>(TContext dbContext) where TContext : DbContext
+  static void InitializeSqlDatabase<TContext>(TContext dbContext) where TContext : DbContext
   {
     dbContext.Database.EnsureDeleted();
     dbContext.Database.EnsureCreated();
@@ -20,17 +20,14 @@ public partial class SqlServerTests
   [AssemblyInitialize]
   public static void InitializeSqlServer(TestContext _)
   {
-    using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(10));
-    var cancellationToken = cancellationTokenSource.Token;
-
     using var entitiesContext = CreateEntitiesContext();
-    InitializeDatabase(entitiesContext);
+    InitializeSqlDatabase(entitiesContext);
 
     using var queriesContext = CreateQueriesContext();
-    InitializeDatabase(queriesContext);
+    InitializeSqlDatabase(queriesContext);
 
        using var trackingContext = CreateTrackingContext();
-    InitializeDatabase(trackingContext);
+    InitializeSqlDatabase(trackingContext);
   }
 
 
