06505a800106c946e86c6ac8f95aca202d54151b
Dragos Tudor <dragos.tudor@gmail.com>
2026-06-23 15:20:19 +0000

    use new mongo database creation instead of the obsolete one.

--- Storing.MongoDb/TestDatabases/Getting.Tests.cs
+++
@@ -1,7 +0,0 @@
-
-namespace Storing.MongoDb;
-
-partial class MongoDbTests
-{
-  static IMongoDatabase GetMongoDatabase(MongoClient client, string dbName) => client.GetDatabase(dbName);
-}
--- Storing.MongoDb/Using.Tests.cs
+++ Storing.MongoDb/Using.Tests.cs
@@ -7,7 +7,7 @@ namespace Storing.MongoDb;
 [TestClass]
 public partial class MongoDbTests
 {
-  static readonly IMongoDatabase Database = GetMongoDatabase(CreateMongoClient("mongodb://127.0.0.1:27017"), "storing");
+  static readonly IMongoDatabase Database = GetMongoDatabase(CreateMongoClient(CreateMongoClientSettings(["127.0.0.1:27017"])), "storing");
 
   [AssemblyInitialize]
   public static void InitializeMongoServer(TestContext _)
