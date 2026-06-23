ec28d925b03b26e7034f34426e081b4002320be9
Dragos Tudor <dragos.tudor@gmail.com>
2026-06-23 14:50:42 +0000

    add convenient Redis database function.

---
+++ Storing.Redis/RedisDatabases/Getting..cs
@@ -0,0 +1,7 @@
+
+namespace Storing.Redis;
+
+partial class RedisFuncs
+{
+  public static IDatabase GetRedisDatabase(IConnectionMultiplexer client, int databaseId = 0) => client.GetDatabase(databaseId);
+}
--- Storing.Redis/TestDatabases/Getting.Tests.cs
+++
@@ -1,7 +0,0 @@
-
-namespace Storing.Redis;
-
-partial class RedisTests
-{
-  static IDatabase GetRedisDatabase(IConnectionMultiplexer client, int databaseId = 0) => client.GetDatabase(databaseId);
-}
